010a1791bf1000f43539f36d7c61d1701069809e
welmentor <welmentor@gmail.com>
2026-02-26 12:03:47 +0530

    Remove dotenv for Render

--- notifier.py
+++ notifier.py
@@ -2,11 +2,8 @@ import time
 import requests
 import os
 import random
-from dotenv import load_dotenv
 from flask import Flask
 from threading import Thread
-
-load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
